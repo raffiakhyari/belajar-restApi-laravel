fbec66adf0ad9554cb05cfd4ec9342a5f7cd9e67
raffiakhyari <muhammadraffi.akhyari@gmail.com>
2021-04-22 15:24:20 +0700

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $user = User::where('email', $request ->email)->first();
+
+        if(! $user || !Hash::check($request->password, $user->password)){
+            return response()->json([
+                'message'=> 'Unauthorized'
+            ],401);
+        }
+
+        $token = $user->createToken('token-name')->plainTextToken;
+
+        return response()->json([
+            'message' => 'succes',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+}
